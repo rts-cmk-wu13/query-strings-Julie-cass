aac1469435b73a05f28b0a2c450de0fc47d76ea3
Julie Bissø <U2507981@rts-365.dk>
2025-02-17 12:07:52 +0100

    more css

--- destinations.js
+++ destinations.js
@@ -7,5 +7,5 @@ console.log(params)
 let id = params.get("id")
 
 fetch(`/data/${id}.json`)
-.then(response => response.json())
-.then (data =>(console.log(data)))
+    .then(response => response.json())
+    .then(data => (console.log(data)))
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
+
 fetch("/data/destinations.json")
-.then(response =>response.json())
-.then(data =>{
-    let sectionElm = document.createElement("section")
-sectionElm.innerHTML = data.destinations.map(destination =>`
-    <img src="/img/${destination.image}" alt="">
+    .then(response => response.json())
+    .then(data => {
+        let sectionElm = document.createElement("section")
+        sectionElm.innerHTML = data.destinations.map(destination =>
+            `<div><img src="/img/${destination.image}" alt="">
     <a href="destinations.html?id=${destination.id}">
-   More
-    </a>`).join("")
+   MORE</a></div>`)
+            .join("")
 
 
-document.querySelector("#root").append(sectionElm)
-})
+        document.querySelector("#root").append(sectionElm)
+    })
